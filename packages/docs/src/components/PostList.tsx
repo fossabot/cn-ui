8e8f56c9ccc3528230971025ad4d5d61a3271165
3446798488 <3446798488@qq.com>
2024-03-29 03:54:04 +0000

    docs: add PostList

---
+++ packages/docs/src/components/PostList.tsx
@@ -0,0 +1,15 @@
+export const PostList = (props: { posts: any[]; prefix: string }) => {
+    return (
+        <ul class="grid grid-cols-24 p-4">
+            {props.posts.map((post) => (
+                <li class="col-span-12 font-bold">
+                    <a href={`${props.prefix}/${post.slug}/`} class="block hover:bg-gray-200 transition-transform rounded-md p-2 ">
+                        <h4 class="text-xl py-1">{post.data.title}</h4>
+                        <p class="font-light text-sm text-design-h2">{post.data.description}</p>
+                        <p class="font-light text-sm text-design-h2">{post.data.pubDate}</p>
+                    </a>
+                </li>
+            ))}
+        </ul>
+    )
+}
