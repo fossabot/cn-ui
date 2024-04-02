296f1192464fb4bed351028e26f47a6418551051
3446798488 <3446798488@qq.com>
2024-04-02 01:43:12 +0000

    docs(button): 更新 Button

--- packages/docs/src/example/ButtonDangerExample.tsx
+++ packages/docs/src/example/ButtonDangerExample.tsx
@@ -1,12 +1,39 @@
-import { Button } from '@cn-ui/core'
+import { Alert, Button, Message, Popover } from '@cn-ui/core'
+import { atom } from '@cn-ui/reactive'
 
 export const ButtonDangerExample = () => {
+    const randomId = 'r-32728290'
+    const show = atom(true)
     return (
         <div>
-            <div class="mb-4 flex gap-4">
-                <Button danger type="primary">
+            <div class="mb-18 flex gap-4">
+                <Button id={randomId} danger type="primary">
                     Primary
                 </Button>
+                <Popover
+                    popoverTarget={'#' + randomId}
+                    v-model={show}
+                    content={(context) => {
+                        const { model } = context!
+                        return (
+                            <div class="flex flex-col">
+                                <div class="flex justify-end gap-4">
+                                    <Button onclick={() => model(false)}>取消</Button>
+                                    <Button
+                                        danger
+                                        type="primary"
+                                        onclick={() => {
+                                            model(false)
+                                            Message.success('删除操作成功')
+                                        }}
+                                    >
+                                        确认
+                                    </Button>
+                                </div>
+                            </div>
+                        )
+                    }}
+                ></Popover>
                 <Button danger>Default</Button>
                 <Button danger type="dashed">
                     Dash
@@ -18,7 +45,7 @@ export const ButtonDangerExample = () => {
                     Link
                 </Button>
             </div>
-            <div class="mb-4 flex gap-4">
+            <div class="mb-14 flex gap-4">
                 <Button danger disabled type="primary">
                     Primary
                 </Button>
