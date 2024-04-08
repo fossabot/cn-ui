b26831e4593b39bbc41d0b30c12333924f9b7d1b
3446798488 <3446798488@qq.com>
2024-04-08 01:55:27 +0000

    feat(reactive): 新增 Portal 的选择控制封装

---
+++ packages/reactive/src/DOM/PortalEasy.tsx
@@ -0,0 +1,10 @@
+import { Show } from 'solid-js'
+import { Portal } from 'solid-js/web'
+
+export function PortalEasy(props: { portalled?: boolean } & Parameters<typeof Portal>[0]) {
+    return (
+        <Show when={props.portalled} fallback={props.children}>
+            <Portal {...props}></Portal>
+        </Show>
+    )
+}
